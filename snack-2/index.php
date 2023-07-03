e2a5c02f13d3b04f2f23ecb658a932157157c44d
Oxidra <dantelezzo@gmail.com>
2023-07-03 13:04:40 +0200

    completato snack-2

---
+++ snack-2/index.php
@@ -0,0 +1,30 @@
+<?php
+    $name = $_GET['name'];
+    $age =  $_GET['age'];
+    $mail = $_GET['mail'];
+
+    if(is_numeric($age) && strlen($name) == 3 && strpos($mail, '.') !== false && strpos($mail, '@') !== false){
+        echo "Accesso riuscito";
+    }else{
+        echo "Accesso negato";
+    }
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Snack-2</title>
+    </head>
+    <body>
+        <form action="./index.php" method="get">
+            <input type="text" name="name" id="name" placeholder="Inserisci il nome">
+            <input type="text" name="mail" id="mail" placeholder="Inserisci una mail">
+            <input type="text" name="age" id="age" placeholder="Inserisci l'età">
+            <button type="submit">Invia</button>
+        </form>
+    </body>
+</html>
