71cb062075fd1724995c2cc21715ff5b78d3c7d0
Oxidra <dantelezzo@gmail.com>
2023-07-04 12:30:02 +0200

    removed unused html

--- snack-3/index.php
+++ snack-3/index.php
@@ -13,16 +13,3 @@
     
     arrayMerge($array1, $array2);
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Snack 3</title>
-    </head>
-    <body>
-        <p>
-        </p>
-    </body>
-</html>
